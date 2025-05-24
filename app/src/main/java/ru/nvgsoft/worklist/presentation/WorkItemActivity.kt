e5ebff7ab1d28d70fff3762c66105878569c5a64
Daniil Shcherbakov <Charel@bk.ru>
2025-05-24 09:32:59 +0300

    create WorkItemActivity, WorkItemViewModel, activity_work_item.xml

---
+++ app/src/main/java/ru/nvgsoft/worklist/presentation/WorkItemActivity.kt
@@ -0,0 +1,15 @@
+package ru.nvgsoft.worklist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.nvgsoft.worklist.R
+
+class WorkItemActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_item)
+
+
+    }
+}
---
+++ app/src/main/java/ru/nvgsoft/worklist/presentation/WorkItemViewModel.kt
@@ -0,0 +1,92 @@
+package ru.nvgsoft.worklist.presentation
+
+import androidx.lifecycle.ViewModel
+import ru.nvgsoft.worklist.data.WorkListRepositoryImpl
+import ru.nvgsoft.worklist.domain.AddWorkItemUseCase
+import ru.nvgsoft.worklist.domain.EditWorkItemUSeCase
+import ru.nvgsoft.worklist.domain.GetWorkItemUseCase
+import ru.nvgsoft.worklist.domain.WorkItem
+
+class WorkItemViewModel: ViewModel() {
+
+    private val repository = WorkListRepositoryImpl() //temp
+
+    private val addWorkItemUseCase = AddWorkItemUseCase(repository)
+    private val editWorkItemUSeCase = EditWorkItemUSeCase(repository)
+    private val getWorkItemUseCase = GetWorkItemUseCase(repository)
+
+
+    fun addWorkItem(
+        date: String?,
+        worker: String?,
+        organisation: String?,
+        description: String?,
+        spendTime: String?
+    ){
+        val newDate = parseDate(date)
+        val newWorker = worker ?: ""
+        val newOrganisation = organisation ?: ""
+        val newDescription = description ?: ""
+        val newSpendTime = parseSpendTime(spendTime)
+        if (validateInputData(newDate, newWorker, newOrganisation, newSpendTime)) {
+            addWorkItemUseCase.addWorkItem(
+                WorkItem(
+                    date = newDate,
+                    worker = newWorker,
+                    organisation = newOrganisation,
+                    description = newDescription,
+                    spendTime = newSpendTime
+                )
+            )
+        }
+    }
+
+    private fun getWorkItem(itemId: Int): WorkItem{
+        return getWorkItemUseCase.getWorkItem(itemId)
+    }
+
+
+    private fun parseDate(date: String?): Long{
+        return try {
+            date?.toLong() ?:0
+        }catch (e: Exception){
+            0
+        }
+    }
+
+    private fun parseSpendTime(spendTime: String?): Double{
+        return try {
+            spendTime?.toDouble() ?: 0.0
+        } catch (e: Exception) {
+            0.0
+        }
+    }
+
+    private fun validateInputData(
+        date: Long,
+        worker: String,
+        organisation: String,
+        spendTime: Double
+    ): Boolean{
+        var result = true
+        if (date <= 0){
+            //TODO input date error
+            result = false
+        }
+        if (worker.isBlank()){
+            //TODO input worker error
+            result = false
+        }
+        if(organisation.isBlank()){
+            //TODO input organisation error
+            result = false
+        }
+         if (spendTime <= 0) {
+             ////TODO input spend time error
+             result = false
+         }
+        return result
+    }
+
+
+}
